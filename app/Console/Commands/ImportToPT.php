bb7cd3feb234cee97c9f04e146c25dcd92a67baf
Thompson <trevort@hpe.com>
2016-03-14 22:56:09 -0500

    finished commit

--- app/Console/Commands/ImportToPT.php
+++ app/Console/Commands/ImportToPT.php
@@ -35,7 +35,8 @@ class ImportToPT extends Command
     {
         parent::__construct();
     }
-
+    public $totalAdded = 0;
+    public $totalSkipped = 0;
     /**
      * Execute the console command.
      *
@@ -43,65 +44,109 @@ class ImportToPT extends Command
      */
     public function handle()
     {
-        //$bcUsername = $this->ask('What is your Basecamp username (email) ?');
-        //$bcPassword = $this->secret('What is your Basecamp password');
+        $bcUsername = $this->ask('What is your Basecamp username (email) ?');
+        $bcPassword = $this->secret('What is your Basecamp password');
 
         $client = \Basecamp\BasecampClient::factory(array(
             'auth' => 'http',
-            //'username' => $bcUsername,
-           // 'password' => $bcPassword,
-            'username' => 'trevor.m.thompson@gmail.com',
-            'password'=> 'trev0802',
+            'username' => $bcUsername,
+            'password' => $bcPassword,
             'user_id' =>  3310361,
             'app_name' => 'test application',
             'app_contact' => 'http://trevorovert.com'
         ));
         $bcProjectId = (int)$this->argument('basecamp_project_id');
 
-        /*To be defined by user, possibly as an input? */
-        $apiToken = '8889d0f2904d48794597f604f9a5b526';
-        $ptProjectId = (int)$this->argument('pivotal_tracker_project_id');
-
-        $response = Curl::to("https://www.pivotaltracker.com/services/v5/projects/".$ptProjectId."/stories")
-                            ->withHeader('X-TrackerToken: '.$apiToken)
-                            ->withOption('SSL_VERIFYPEER', false)
-                            ->asJson()
-                            ->get();
-
-        print_r($response); exit(0);
-
-
-
         //If the "To dos assigned to" option was selected, then we will hit that api function
         if($this->option('to_dos_assigned_to')){
-            $response = $client->getAssignedTodolistsByPerson(array(
+            $lists = $client->getAssignedTodolistsByPerson(array(
                 'personId' => (int)$this->option('to_dos_assigned_to')
                 ));
+            //For loops break down the todo lists into individual todos. Then build the response array.
+            foreach($lists as $list){
+                foreach($list["assigned_todos"] as $todo){
+                    $response[] = $todo;
+                }
+            }
         }else if($this->option('to_do_list_id')){
-            //gathers to-dos from a specified todo list id
-            $response = $client->getTodoList(array(
+            //gathers to-dos from a specified todo list id. Function was custom added to service.php
+            $response = $client->getTodosByList(array(
                 'projectId' => $bcProjectId,
                 'todolistId' => (int)$this->option('to_do_list_id')
                 ));
+            print_r($response); exit(0);
         }else{
-            //No options selected, standard pull by project
-            $response = $client->getTodolistsByProject( array( 
+            //No options selected, standard pull by project. This will return all todos in a project.
+            //Function was custom added to service.php in basecamp api
+            $response = $client->getTodosByProject( array( 
                 'projectId' => $bcProjectId, 
              ) );
-            foreach($response as $list){
-                $todo[] = $client->getTodolist(array(
-                    'projectId' => $bcProjectId,
-                    'todolistId' => $list["id"]
-                ));
-                 $response = Curl::to("https://www.pivotaltracker.com/services/v5/projects/".$ptProjectId."/stories")
-                            ->withHeader('X-TrackerToken: '.$apiToken)
-                            ->withOption('SSL_VERIFYPEER', false)
-                            ->asJson()
-                            ->post();
-                //Place 
-                
-            }
-            print_r($todo);
         }
+
+        //Generate array of stories using bc data
+        $insertArray = $this->createOutputArray($response);
+        //Curls array into PT.
+        $this->insertPT($insertArray);
+
+        $this->info('Job completed');
+        $this->info('Number of stories added: ' .$this->totalAdded);
+        $this->info('Number of stories skipped due to already existing: '.$this->totalSkipped);
+
     }
+        /*****
+        *
+        * Generate an array that contains data from basecamp ($list) 
+        *       in PT format ($insertArray @return)
+        *
+        */
+        public function createOutputArray($list){
+            $listIterator = 0;
+            foreach($list as $item){
+                $insertArray[$listIterator]["name"] = $item["content"];
+                $insertArray[$listIterator]["labels"][] = "BC-IMPORT-".$item["id"];
+                $listIterator++;
+            }
+            return $insertArray;
+        }
+
+
+        /*****
+        *
+        * Inserts an array into PT stories.
+        *
+        */
+        public function insertPT($insertArray){
+            //Project ID for PT from Input
+            $ptProjectId = (int)$this->argument('pivotal_tracker_project_id');
+            //Api token specific to user
+            $apiToken = '8889d0f2904d48794597f604f9a5b526';
+            $bar = $this->output->createProgressBar(count($insertArray));
+            foreach($insertArray as $insert){
+                /*First check if it already exists*/
+                 $checkExists = Curl::to("https://www.pivotaltracker.com/services/v5/projects/".$ptProjectId."/stories?with_label=".$insert["labels"][0])
+                                ->withHeader('X-TrackerToken: '.$apiToken)
+                                ->withHeader('Content-Type: application/json')
+                                ->withOption('SSL_VERIFYPEER', false)
+                                ->asJson()
+                                ->get(); 
+
+                //If it doesn't exist (verified by using the label and ID from bc) then go ahead and insert it.
+                if(!$checkExists){
+                    $response = Curl::to("https://www.pivotaltracker.com/services/v5/projects/".$ptProjectId."/stories")
+                                    ->withHeader('X-TrackerToken: '.$apiToken)
+                                    ->withHeader('Content-Type: application/json')
+                                    ->withOption('SSL_VERIFYPEER', false)
+                                    ->withData($insert)
+                                    ->asJson()
+                                    ->post(); 
+                    //Record how many were added each time.
+                    $this->totalAdded++;
+                }else{
+                    //Record how many we skipped because they were already imported.
+                    $this->totalSkipped++;
+                }
+                $bar->advance();
+            }
+            $bar->finish();
+        }
 }
